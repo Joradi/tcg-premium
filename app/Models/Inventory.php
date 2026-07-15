68281fe79887f2850ef2d54a3827d2ae91941c35
jonat <jona.diaz.m@gmail.com>
2026-07-15 12:19:40 -0400

    fix: persist inventory variant

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -7,7 +7,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Inventory extends Model
 {
-    protected $fillable = ['card_id', 'language', 'condition', 'price', 'stock', 'is_active', 'visits'];
+    protected $fillable = [
+        'card_id',
+        'language',
+        'condition',
+        'variant',
+        'price',
+        'stock',
+        'is_active',
+        'visits',
+    ];
 
     public function card(): BelongsTo
     {
